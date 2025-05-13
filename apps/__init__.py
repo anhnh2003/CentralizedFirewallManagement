325496cfa0f96c3e2fb923f10a965b7326588f33
anhnh <anhnh198@os.viettel.com>
2025-05-13 10:47:36 +0700

    fix syntax

--- apps/__init__.py
+++ apps/__init__.py
@@ -4,37 +4,40 @@ from flask import Flask, redirect, url_for
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
-from apps.authentication.models import Users
-
 
 db = SQLAlchemy()
-
 login_manager = LoginManager()
-@login_manager.user_loader
-def load_user(user_id):
-    return Users.query.get(int(user_id))
-# Specify the login view (redirect for unauthorized users)
+
+# Cấu hình Flask-Login
 login_manager.login_view = 'authentication_blueprint.login'
 login_manager.login_message = 'Please log in to access this page.'
-login_manager.login_message_category = 'info'  # Optional: category for flash messages
+login_manager.login_message_category = 'info'  # Optional
 
 def register_extensions(app):
+    db.init_app(app)
     login_manager.init_app(app)
 
+    # Import tại đây để tránh circular import
+    from apps.models import Users
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
 
 def register_blueprints(app):
     for module_name in ('authentication', 'home'):
-        module = import_module('apps.{}.routes'.format(module_name))
-        app.register_blueprint(module.blueprint, url_prefix='/{}'.format(module_name))
-
-
+        module = import_module(f'apps.{module_name}.routes')
+        app.register_blueprint(module.blueprint, url_prefix=f'/{module_name}')
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
+
     register_extensions(app)
     register_blueprints(app)
+
     @app.route('/')
     def default():
         return redirect(url_for('authentication_blueprint.login'))
+
     return app
